58aa74ee9e62730863722a33df8110185246cf37
kenghow777 <93174619+kenghow777@users.noreply.github.com>
2023-06-09 17:41:56 +0700

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import 'zone.js/dist/zone';
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormGroup, FormArray, FormControl } from '@angular/forms';
+import { FormGroup, FormArray, FormControl, Form } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'my-app',
   standalone: true,
   imports: [CommonModule],
-  template: ``,
+  template: `
+  <form [formGroup]="form" (ngSubmit)="onSubmit()">
+  <ng-container *ngFor="let control of form.controls['subCategories'].controls">
+    <ng-container [formGroup]="control">
+      <input type="text" formControlName="name">
+      <div formArrayName="subCategories">
+        <ng-container *ngFor="let subControl of control.controls['subCategories'].controls">
+          <ng-container [formGroup]="subControl">
+            <input type="text" formControlName="name">
+          </ng-container>
+        </ng-container>
+      </div>
+    </ng-container>
+  </ng-container>
+  <button type="submit">Submit</button>
+</form>
+  `,
 })
 export class App implements OnInit {
+  form!: FormGroup;
+
   ngOnInit(): void {
     let categoryData = {
       name: 'Main',
@@ -23,7 +41,26 @@ export class App implements OnInit {
       ],
     };
 
-    this.form = this.createFormgroup(categoryData)
+    this.form = this.createFormGroup(categoryData);
+  }
+
+  createFormGroup(categoryData: any): FormGroup {
+    let formGroup = new FormGroup({
+      name: new FormControl(categoryData.name),
+      subCategories: new FormArray([]),
+    });
+
+    for (let subCategory of categoryData.subCategories) {
+      (formGroup.get('subCategories') as FormArray).push(
+        this.createFormGroup(subCategory)
+      );
+    }
+
+    return formGroup;
+  }
+
+  onSubmit(): void {
+    console.log(this.form.value);
   }
 }
 
